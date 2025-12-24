d0882318fce35ddda21597144ef16804acf84f9f
Stefan <1119788227@qq.com>
2025-12-24 18:14:21 +0800

    feat: 修复部署问题并归档旧资源
    - 修正了网页管理端WebSocket连接地址，解决了登录后页面异常的问题。
    
    - 为最终部署包中的Nginx配置添加了WebSocket代理支持。
    
    - 将旧的部署资源和未使用的小程序源码移动到归档目录，以清理项目结构。

--- project-rjwm-weixin-uniapp-develop-wsy/utils/env.js
+++
@@ -1,8 +0,0 @@
-
-// 测试环境
-// export const baseUrl = 'http://reggie-dev.itheima.net'
-// export const baseUrl = 'http://172.16.43.24:8080'
-// export const baseUrl ='https://reggie-parent-t.itheima.net'
-export const baseUrl = 'http://66.154.127.152/api'
-// 线上环境
-// export const baseUrl = 'https://registakeaway.itheima.net'
